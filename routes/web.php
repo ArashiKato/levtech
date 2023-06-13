3b08a480c539b43cfc19f4869283ef25f41318ce
ArashiKato <arashi61922@icloud.com>
2023-06-13 12:39:06 +0900

    add create button

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ use App\Http\Controllers\PostController;
 //     return view('posts.index');
 // });
 Route::get('/', [PostController::class, 'index']); 
+Route::get('/posts/create', [PostController::class ,'create']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
 
 // '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
